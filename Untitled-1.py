298a0e2d7f393440dd189a78e02b087c2aeb6596
RH9-A <147848030+RH9-A@users.noreply.github.com>
2023-10-13 19:05:01 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+name = input("what is your name?")
+age = input("what is your age?")
+
+input("your name is " + name + " and your age is " + age + "is that correct?" )
+
+
+no = "no"
+
+
+if input(no):
+    print("fuck you")
+else:
+    print("ok thats cool")    
+       
